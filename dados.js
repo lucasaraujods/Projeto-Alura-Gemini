15af3b0f5b5fa3bb8bb1bacebd868c16a61a1825
lucasaraujods <lucasara1707@gmail.com>
2024-09-06 19:45:01 -0300

    alimentado a base de dados com objetos

---
+++ dados.js
@@ -0,0 +1,49 @@
+//criando um objeto de dados, com as propriedades de nome, descricao e link de execução do exercício
+let dados = [
+    {
+
+        titulo: "Supino reto",
+        descricao: "O supino reto é um exercício composto, fundamental para o desenvolvimento da musculatura do peito, mais especificamente do peitoral maior. Ele também trabalha os músculos deltóides (ombros) e tríceps braquial de forma secundária.",
+        dica: "A coluna deve estar sempre apoiada no banco e os pés firmes no chão",
+        link: "https://youtu.be/EZMYCLKuGow?si=Tg1DjB6VCH476soT",
+        //adicionamos uma tags de busca 
+        tags: "peito peitoral superior"
+    },
+    {
+
+        titulo: "Supino inclinado",
+        descricao: "é uma variação do supino tradicional que coloca um maior foco na parte superior do músculo peitoral. Ao inclinar o banco, você altera o ângulo de trabalho dos músculos, permitindo um recrutamento maior das fibras musculares superiores",
+        dica: "Experimente diferentes ângulos de inclinação para encontrar o que proporciona o melhor estímulo para seus músculos.",
+        link: "https://youtu.be/WP1VLAt8hbM?si=uBBd7ZS72HIdhrFt",
+        //adicionamos uma tags de busca 
+        tags: "peito peitoral superior"
+    },
+    {
+
+        titulo: "supino declinado",
+        descricao: "Ao realizar esse exercício em um banco declinado, você aumenta o foco na parte inferior do peitoral, proporcionando uma variação interessante para quem busca desenvolver essa área de forma mais completa",
+        dica: "Mantenha o movimento controlado, tanto na descida quanto na subida, para maximizar o trabalho muscular.",
+        link: "https://youtu.be/J2g6qPBJfqo?si=oOpbG89e2qKIMUGm",
+        //adicionamos uma tags de busca 
+        tags: "peito peitoral superior declinado"
+    },
+    {
+
+        titulo: "cross over",
+        descricao: "é um exercício de isolamento que permite trabalhar os músculos peitorais de forma mais localizada, proporcionando um maior foco na parte interna do peito. Ele é excelente para definir e dar um acabamento mais estético aos músculos.",
+        dica: "Realize o movimento completo, contraindo os músculos peitorais no ponto máximo da contração.",
+        link: "https://youtu.be/jqTlJt3JXzQ?si=tJaW9fIpgvbqqQlf",
+        //adicionamos uma tags de busca 
+        tags: "peito peitoral superior cross polia"
+    },
+    {
+
+        titulo: "crucifixo máquina",
+        descricao: "é um exercício de isolamento que permite trabalhar os músculos peitorais de forma mais localizada, com foco na parte interna e externa do peito. O crucifixo na máquina oferece maior estabilidade e permite um movimento mais controlado.",
+        dica: "Mantenha as costas apoiadas no encosto e os ombros relaxados durante todo o movimento.",
+        link: "https://youtu.be/FzCnfD0gOXo?si=piMifNG675EgJM03",
+        //adicionamos uma tags de busca 
+        tags: "peito peitoral superior peck deck crucifixo"
+    },
+    
+];
